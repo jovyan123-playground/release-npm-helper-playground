2921c0646c5bf5a84ae14825ba6d899563a4a91d
Jovyan 123 <jovyan123.playground@gmail.com>
2021-03-10 01:51:23 -0600

    finish verdaccio runner

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-// Log to ~/.
 const fs = require('fs');
 const child_process = require('child_process');
 const path = require('path');
 const os = require('os');
 const process = require('process');
+const psList = require('ps-list');
 
-
-const custom_registry_url = "http://localhost:4877"
-const original_npm_registry_url = `npm get registry`
-const original_yarn_registry_url = `yarn config get registry`
 const default_verdaccio_package = "verdaccio@^4.5.1";
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 async function startLocalRegistry() {
+    await stopLocalRegistry();
+
     // make a temp dir to run verdaccio
     const temp_dir = fs.mkdtempSync(path.join(os.tmpdir(), 'verdaccio_'))
 
@@ -23,16 +21,25 @@ async function startLocalRegistry() {
     const pkg_name = default_verdaccio_package;
 
     // Start local registry
-    const cmd = `npx --yes -- ${pkg_name} -c verdaccio.yml -l localhost:4877`;
-    console.log('Running in', temp_dir);
-    console.log('>', cmd);
+    let port = 5559;
+    console.log('Installing', pkg_name);
+    child_process.execSync(`npm install -g ${pkg_name}`)
 
-    child_process.spawnSync(cmd, { cwd: temp_dir, detached: true });
+    const args = `-c verdaccio.yml -l localhost:${port}`;
+    console.log('Running in', temp_dir);
+    console.log('>', 'verdaccio', args);
+
+    const out = fs.openSync(log_file, 'a');
+    const err = fs.openSync(log_file, 'a');
+
+    const options = { cwd: temp_dir, detached: true, stdio: ['ignore', out, err] };
+    const subproc = child_process.spawn('verdaccio', args.split(' '), options);
+    subproc.unref();
 
     // Wait for Verdaccio to boot
     let content = '';
     while (true) {
-        console.log(log_file, content);
+        process.stdout.write('.');
         if (content.indexOf('http address') !== -1) {
             break
         }
@@ -45,17 +52,37 @@ async function startLocalRegistry() {
             content = fs.readFileSync(log_file, encoding = 'utf-8');
         }
     }
+    console.log('\nverdaccio started');
 
     // Set registry to local registry
-    child_process.execFileSync(`npm set registry "${custom_registry_url}"`)
-    child_process.execFileSync(`yarn config set registry "${custom_registry_url}"`)
+    const custom_registry_url = `http://localhost:${port}`;
+    child_process.execSync(`npm set registry "${custom_registry_url}"`)
+    try {
+        child_process.execSync(`yarn config set registry "${custom_registry_url}"`)
+    } catch (e) {
+        // yarn not available
+    }
+    process.exit(0);
 }
 
-// function stopLocalRegistry {
-//   # Restore the original NPM and Yarn registry URLs and stop Verdaccio
-//     npm set registry "$original_npm_registry_url"
-//     yarn config set registry "$original_yarn_registry_url"
-// }
+
+// Restore the original NPM and Yarn registry URLs and stop Verdaccio
+async function stopLocalRegistry() {
+    child_process.execSync('npm config rm registry');
+    try {
+        child_process.execSync(`yarn config delete registry "${custom_registry_url}"`)
+    } catch (e) {
+        // yarn not available
+    }
+    const list = await psList();
+    list.forEach(info => {
+        if (info.name.indexOf('verdaccio') !== -1) {
+            console.log('killing', info.name, info.pid);
+            process.kill(info.pid);
+        }
+    })
+}
+
 
 (async function () {
     await startLocalRegistry();
